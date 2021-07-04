08171cb4ac27e4c6b1c9842a4bd3fe32b83552fb
omkargb <omkargb107@gmail.com>
2021-07-04 23:58:56 +0530

    Updated : Checking employee attendance

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,4 +1,17 @@
 #!/bin/bash -x
 #created by : Mr. Omkar G Bapat
 
-Welcome to Employee Wage Computation Program
+echo " Welcome to Employee Wage Computation Program "
+
+#UC1 : Checking employee attendance
+
+ispresent=1
+
+randomCheck=$((RANDOM%2))
+
+if [ $ispresent -eq $randomCheck ]
+then
+	echo " Employee is present. "
+else
+	echo " Employee is absent. "
+fi
