7a1e19bc1ec9d9f10b2da2b8557fa2ec189f2236
omkargb <omkargb107@gmail.com>
2021-07-05 23:18:05 +0530

    Updated : Employee Part time present or not, attendance check and wage calculations

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,19 +1,31 @@
-#!/bin/bash -x
+# !/bin/bash -x
 #created by : Mr. Omkar G Bapat
 
 echo " Welcome to Employee Wage Computation Program "
 
-ispresent=1
-workHrs=8
 wagePerHr=20
 
-randomCheck=$((RANDOM%2))
+isFullTime=2
+isPartTime=1
+isAbsent=0
 
-if [ $ispresent -eq $randomCheck ]
+randomCheck=$((RANDOM%3))
+
+echo " Checking employee attendance and calculating day salary "
+
+if [ $isFullTime -eq $randomCheck ]
 then
-	echo " Employee is present. "
-	salary=$(($wagePerHr * $workHrs))
+	echo " Employee is present Full Time. "
+	workHrs=8
+elif [ $isPartTime -eq $randomCheck ]
+then
+	echo " Employee is present Part Time. "
+	workHrs=4
 else
-	echo " Employee is absent. "
-	salary=0
+        echo " Employee is absent. "
+        workHrs=0
 fi
+
+salary=$(($wagePerHr * $workHrs))
+
+echo " Day Salary : $salary"
